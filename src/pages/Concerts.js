0765caf00cb92a8c93bd73d506862c91979d9c55
qkrtjdrb <qkrtjdrb321@naver.com>
2024-12-19 09:57:16 +0900

    modified:   package.json 	modified:   src/pages/Concerts.js

--- src/pages/Concerts.js
+++ src/pages/Concerts.js
@@ -10,7 +10,7 @@ function Concerts() {
   useEffect(() => {
     // 데이터 가져오기
     axios
-      .get("http://10.0.5.173:5000/Concerts")
+      .get("/Concerts")
       .then((response) => {
         setConcerts(response.data);
       })
